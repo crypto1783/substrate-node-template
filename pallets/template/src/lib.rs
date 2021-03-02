8807642e07a9c43eedc59a5234750d94becde1d0
jianlexia <56173042@qq.com>
2021-03-02 14:25:12 +0800

    benchmarks

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,128 +1,91 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+/// A FRAME pallet for demo benchmark
+
 use frame_support::{
-	decl_module, decl_storage, decl_event, decl_error, ensure, StorageMap,dispatch,
+	decl_module, decl_storage, decl_event, decl_error, dispatch,
+	traits::{
+		Get
+	},
 };
-use frame_system::ensure_signed;
-use sp_std::vec::Vec;
+use frame_system::{self as system, ensure_signed};
 
-// 2. Configuration
-/// Configure the pallet by specifying the parameters and types on which it depends.
-pub trait Trait: frame_system::Trait {
-	/// Because this pallet emits events, it depends on the runtime's definition of an event.
-	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+mod beanchmarking;
+
+/// The pallet's configuration trait.
+pub trait Trait: system::Trait {
+	// Add other types and constants required to configure this pallet.
+
+	/// The overarching event type.
+	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
-// 3. Storage
-// The pallet's runtime storage items.
-// https://substrate.dev/docs/en/knowledgebase/runtime/storage
+// This pallet's storage items.
 decl_storage! {
-    trait Store for Module<T: Trait> as TemplateModule {
-        /// The storage item for our proofs.
-        /// It maps a proof to the user who made the claim and when they made it.
-        Proofs: map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
-    }
+	// It is important to update your storage name so that your pallet's
+	// storage items are isolated from other pallets.
+	// ---------------------------------vvvvvvvvvvvvvv
+	trait Store for Module<T: Trait> as TemplateModule {
+		// Just a dummy storage item.
+		// Here we are declaring a StorageValue, `Something` as a Option<u32>
+		// `get(fn something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
+		Something get(fn something): Option<u32>;
+	}
 }
 
-// 4. Events
-// Pallets use events to inform users when important changes are made.
-// Event documentation should end with an array that provides descriptive names for parameters.
-// https://substrate.dev/docs/en/knowledgebase/runtime/events
-decl_event! {
-    pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
-        /// Event emitted when a proof has been claimed. [who, claim]
-        ClaimCreated(AccountId, Vec<u8>),
-        /// Event emitted when a claim is revoked by the owner. [who, claim]
-        ClaimRevoked(AccountId, Vec<u8>),
-    }
-}
+// The pallet's events
+decl_event!(
+	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
+		/// Just a dummy event.
+		/// Event `Something` is declared with a parameter of the type `u32` and `AccountId`
+		/// To emit this event, we call the deposit function, from our runtime functions
+		SomethingStored(u32, AccountId),
+	}
+);
 
-// 5. Errors
-// Errors inform users that something went wrong.
+// The pallet's errors
 decl_error! {
-    pub enum Error for Module<T: Trait> {
-        /// The proof has already been claimed.
-        ProofAlreadyClaimed,
-        /// The proof does not exist, so it cannot be revoked.
-        NoSuchProof,
-        /// The proof is claimed by another account, so caller can't revoke it.
-        NotProofOwner,
-
-        NotClaimOwner,
-
-        ClaimNotExist,
-    }
+	pub enum Error for Module<T: Trait> {
+		/// Value was None
+		NoneValue,
+		/// Value reached maximum and cannot be incremented further
+		StorageOverflow,
+	}
 }
 
-// 6. Callable Functions
-// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-// These functions materialize as "extrinsics", which are often compared to transactions.
-// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+// The pallet's dispatchable functions.
 decl_module! {
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        // Errors must be initialized if they are used by the pallet.
-        type Error = Error<T>;
+	/// The module declaration.
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		// Initializing errors
+		// this includes information about your errors in the node's metadata.
+		// it is needed only if you are using errors in your pallet
+		type Error = Error<T>;
 
-        // Events must be initialized if they are used by the pallet.
-        fn deposit_event() = default;
+		// Initializing events
+		// this is needed only if you are using events in your pallet
+		fn deposit_event() = default;
 
-        /// Allow a user to claim ownership of an unclaimed proof.
-        #[weight = 10_000]
-        fn create_claim(origin, proof: Vec<u8>) {
-            // Check that the extrinsic was signed and get the signer.
-            // This function will return an error if the extrinsic is not signed.
-            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
-            let sender = ensure_signed(origin)?;
+		/// Just a dummy entry point.
+		/// function that can be called by the external world as an extrinsics call
+		/// takes a parameter of the type `AccountId`, stores it, and emits an event
+		/// # <weight>
+		/// - Base Weight: 44.66 µs
+		/// - DB Weight: 1 Write 1
+		/// # </weight>
+		#[weight = T::DbWeight::get().writes(1) + 44_660_000]
+		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
+			// Check it was signed and get the signer. See also: ensure_root and ensure_none
+			let who = ensure_signed(origin)?;
 
-            // Verify that the specified proof has not already been claimed.
-            ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+			// Code to execute when something calls this.
+			// For example: the following line stores the passed in u32 in the storage
+			Something::put(something);
 
-            // Get the block number from the FRAME System module.
-            let current_block = <frame_system::Module<T>>::block_number();
+			// Here we are raising the Something event
+			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			Ok(())
+		}
 
-            // Store the proof with the sender and block number.
-            Proofs::<T>::insert(&proof, (&sender, current_block));
-
-            // Emit an event that the claim was created.
-            Self::deposit_event(RawEvent::ClaimCreated(sender, proof));
-        }
-
-        /// Allow the owner to revoke their claim.
-        #[weight = 10_000]
-        fn revoke_claim(origin, proof: Vec<u8>) {
-            // Check that the extrinsic was signed and get the signer.
-            // This function will return an error if the extrinsic is not signed.
-            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
-            let sender = ensure_signed(origin)?;
-
-            // Verify that the specified proof has been claimed.
-            ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
-
-            // Get owner of the claim.
-            let (owner, _) = Proofs::<T>::get(&proof);
-
-            // Verify that sender of the current call is the claim owner.
-            ensure!(sender == owner, Error::<T>::NotProofOwner);
-
-            // Remove claim from storage.
-            Proofs::<T>::remove(&proof);
-
-            // Emit an event that the claim was erased.
-            Self::deposit_event(RawEvent::ClaimRevoked(sender, proof));
-        }
-
-
-        #[weight = 0]
-        pub fn transfer_claim(origin, claim:Vec<u8>, dest:T::AccountId) -> dispatch::DispatchResult{
-
-           let sender = ensure_signed(origin)?;
-           ensure!(Proofs::<T>::contains_key(&claim), Error::<T>::ClaimNotExist);
-           let (owner, _block_number) = Proofs::<T>::get(&claim);
-           ensure!(owner == sender, Error::<T>::NotClaimOwner);
-           Proofs::<T>::insert(&claim,(dest, frame_system::Module::<T>::block_number()));
-           Ok(())
-        }
-
-
-    }
+	}
 }
